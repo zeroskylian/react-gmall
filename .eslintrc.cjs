552e013e9fffb71b4eb2ade36b8f163aa7bdd945
lianxinbo <lianxinbo@hengli.com>
2023-05-11 13:10:37 +0800

    add config

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        parser: 'typescript',
+        semi: true
+      }
+    ],
+    quotes: [1, 'single'],
+    semi: [1, 'always'],
+    'no-extra-semi': 1,
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'none', ignoreRestSiblings: false }
+    ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/jsx-uses-react': 'error',
+    'react/react-in-jsx-scope': 'error'
+  }
+};
