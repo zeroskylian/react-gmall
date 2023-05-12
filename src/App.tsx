54baefa212a6f90b425660c5e4ea43960761cf5f
lianxinbo <lianxinbo@hengli.com>
2023-05-12 10:20:42 +0800

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/reset.css';
 import './css/index.css';
+import './css/common.css';
 import Header from './components/Header';
 
 function App() {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  const navs = [
+    '我的订单',
+    '我的购物车',
+    '我的尚品汇',
+    '尚品汇会员',
+    '企业采购',
+    '关注尚品汇',
+    '招商合作',
+    '商家后台'
+  ];
+  return (
+    <div className="topbar">
+      <div className="container clearfix">
+        <div className="left leftfix">
+          <span className="hello">尚品汇欢迎您</span>
+          <span>请</span>
+          <a className="login" href="#login">
+            登录
+          </a>
+          <a className="register" href="#register">
+            免费注册
+          </a>
+        </div>
+        <div className="right rightfix">
+          <ul>
+            {navs.map((nav) => {
+              return (
+                <li key={nav}>
+                  <a href={`#${nav}`}>{nav}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
